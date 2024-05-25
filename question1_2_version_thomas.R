e283599b78425fa78bc44554b370e9e9ecc81dd7
thomascailteux <61298162+thomascailteux@users.noreply.github.com>
2024-05-25 11:03:41 +0200

    Update question1_2_version_thomas.R

--- question1_2_version_thomas.R
+++ question1_2_version_thomas.R
@@ -2,6 +2,11 @@ library(stats)
 library(ggplot2)
 library(pracma)
 
+# Approximation de la constante d'Euler-Mascheroni par une somme de série
+n.tilt <- 1000000
+gamma_approx <- sum(1 / seq(1, n.tilt)) - log(n.tilt)
+
+
 # Fonction pour initialiser les listes avec des valeurs aléatoires de distribution Gumbel
 initialisation <- function(n) {
   unif.distrib <- runif(n, min = 0, max = 1)
@@ -94,6 +99,23 @@ mu_hat_1000<- numeric(M)
 mu_hat_1250<- numeric(M)
 
 
+# valeur des estimateurs mu des moments
+
+mu_mom_250<- numeric(M)
+mu_mom_500<- numeric(M)
+mu_mom_750<- numeric(M)
+mu_mom_1000<- numeric(M)
+mu_mom_1250<- numeric(M)
+
+# valeur des estimateurs beta des moments
+
+beta_mom_250<- numeric(M)
+beta_mom_500<- numeric(M)
+beta_mom_750<- numeric(M)
+beta_mom_1000<- numeric(M)
+beta_mom_1250<- numeric(M)
+
+
 # Boucle de simulations
 for (i in 1:M) {
   l_250 <- initialisation(250)
@@ -120,17 +142,17 @@ for (i in 1:M) {
   MSEmu_1000[i] <- (res_1000$par[2] - 2)^2
   MSEmu_1250[i] <- (res_1250$par[2] - 2)^2
   
-  B_250[i] <- abs(res_250$par[1] - 3)
-  B_500[i] <- abs(res_500$par[1] - 3)
-  B_750[i] <- abs(res_750$par[1] - 3)
-  B_1000[i] <- abs(res_1000$par[1] - 3)
-  B_1250[i] <- abs(res_1250$par[1] - 3)
+  B_250[i] <- (res_250$par[1] - 3)
+  B_500[i] <- (res_500$par[1] - 3)
+  B_750[i] <- (res_750$par[1] - 3)
+  B_1000[i] <- (res_1000$par[1] - 3)
+  B_1250[i] <- (res_1250$par[1] - 3)
   
-  Bmu_250[i] <- (res_250$par[2] - 2)^2
-  Bmu_500[i] <- (res_500$par[2] - 2)^2
-  Bmu_750[i] <- (res_750$par[2] - 2)^2
-  Bmu_1000[i] <- (res_1000$par[2] - 2)^2
-  Bmu_1250[i] <- (res_1250$par[2] - 2)^2
+  Bmu_250[i] <- (res_250$par[2] - 2)
+  Bmu_500[i] <- (res_500$par[2] - 2)
+  Bmu_750[i] <- (res_750$par[2] - 2)
+  Bmu_1000[i] <- (res_1000$par[2] - 2)
+  Bmu_1250[i] <- (res_1250$par[2] - 2)
   
   
   beta_hat_250[i]<- res_250$par[1]
@@ -146,27 +168,102 @@ for (i in 1:M) {
   mu_hat_1250[i]<-res_1250$par[2]
   
   
+  
+  # calcul des estimateurs des moments
+  
+  beta_mom_250[i]<-sqrt((6/pi^2)*sum(mean(l_250^2)-mean(l_250)^2))
+  beta_mom_500[i]<-sqrt((6/pi^2)*sum(mean(l_500^2)-mean(l_500)^2))
+  beta_mom_750[i]<-sqrt((6/pi^2)*sum(mean(l_750^2)-mean(l_750)^2))
+  beta_mom_1000[i]<-sqrt((6/pi^2)*sum(mean(l_1000^2)-mean(l_1000)^2))
+  beta_mom_1250[i]<-sqrt((6/pi^2)*sum(mean(l_1250^2)-mean(l_1250)^2))
+  
+  mu_mom_250[i]<-mean(l_250) - gamma_approx*beta_mom_250[i]
+  mu_mom_500[i]<-mean(l_500) - gamma_approx*beta_mom_500[i]
+  mu_mom_750[i]<-mean(l_750) - gamma_approx*beta_mom_750[i]
+  mu_mom_1000[i]<-mean(l_1000) - gamma_approx*beta_mom_1000[i]
+  mu_mom_1250[i]<-mean(l_1250) - gamma_approx*beta_mom_1250[i]
+  
+  
 }
 
 
 
+
+
+
 abcis <- c(250, 500, 750, 1000, 1250)
+
+#MSE,biais et variance moyenne pour beta likly
+           
 MSE_tot <- c(mean(MSE_250), mean(MSE_500), mean(MSE_750), mean(MSE_1000), mean(MSE_1250))
 B_tot <- c(mean(B_250), mean(B_500), mean(B_750), mean(B_1000), mean(B_1250))
 V_tot<-c(var(beta_hat_250),var(beta_hat_500),var(beta_hat_750),var(beta_hat_1000),var(beta_hat_1250))
 
-# Tracé des MSE
-plot(abcis, MSE_tot, type="b", col="blue", xlab="Taille de l'échantillon", ylab="MSE", main="MSE en fonction de la taille de l'échantillon")
+#MSE,biais et variance moyenne pour mu likly
 
-# Tracé des biais
-plot(abcis, B_tot, type="b", col="red", xlab="Taille de l'échantillon", ylab="Biais", main="Biais en fonction de la taille de l'échantillon")
+MSE_tot_mu <- c(mean(MSEmu_250), mean(MSEmu_500), mean(MSEmu_750), mean(MSEmu_1000), mean(MSEmu_1250))
+B_tot_mu <- c(mean(Bmu_250), mean(Bmu_500), mean(Bmu_750), mean(Bmu_1000), mean(Bmu_1250))
+V_tot_mu<-c(var(mu_hat_250),var(mu_hat_500),var(mu_hat_750),var(mu_hat_1000),var(mu_hat_1250))
 
-# Tracé de la variance 
-plot(abcis, V_tot, type="b", col="red", xlab="Taille de l'échantillon", ylab="Variance", main="Variance en fonction de la taille de l'échantillon")
 
-# On plot le biais de beta carré + la variance de beta 
+
+# on calcul le biais des estimateurs de moment
+
+B_tot_mom_mu <-c(mean(mu_mom_250 -2 ),mean(mu_mom_500 -2 ),mean(mu_mom_750 -2 ),mean(mu_mom_1000 -2 ),mean(mu_mom_1250 -2 ))
+B_tot_mom_beta <-c(mean(beta_mom_250 -3 ),mean(beta_mom_500 -3 ),mean(beta_mom_750 -3 ),mean(beta_mom_1000 -3 ),mean(beta_mom_1250 -3 ))
+
+
+# on calcul la MSE des estimateurs de moment
+MSE_tot_mom_mu <-c(mean((mu_mom_250 -2)^2 ),mean((mu_mom_500 -2)^2 ),mean((mu_mom_750 -2)^2 ),mean((mu_mom_1000 -2)^2 ),mean((mu_mom_1250 -2 )^2))
+MSE_tot_mom_beta <-c(mean((beta_mom_250 -3)^2 ),mean((beta_mom_500 -3)^2 ),mean((beta_mom_750 -3)^2 ),mean((beta_mom_1000 -3)^2 ),mean((beta_mom_1250 -3)^2 ))
+
+
+# on calcul la variance des estimateurs de moment
+
+Var_tot_mom_mu <-c(var(mu_mom_250),var(mu_mom_500),var(mu_mom_750),var(mu_mom_1000),var(mu_mom_1250))
+Var_tot_mom_beta <-c(var(beta_mom_250),var(beta_mom_500),var(beta_mom_750),var(beta_mom_1000),var(beta_mom_1250))
+
+
+# Tracé des MSE pour beta
+plot(abcis, MSE_tot, type="b", col="blue", xlab="Taille de l'échantillon", ylab="MSE", main="MSE de beta en fonction de la taille de l'échantillon")
+lines(abcis, MSE_tot_mom_beta, col="red")
+legend("topright", legend = c("Likley", "moment"), col = c("blue", "red"), lty = 1)
+
+
+# Tracé des MSE pour mu
+plot(abcis, MSE_tot_mu, type="b", col="blue", xlab="Taille de l'échantillon", ylab="MSE", main="MSE de mu en fonction de la taille de l'échantillon")
+lines(abcis, MSE_tot_mom_beta, col="red")
+legend("topright", legend = c("Likley", "moment"), col = c("blue", "red"), lty = 1)
+
+
+
+
+# Tracé des biais pour beta
+plot(abcis, B_tot, type="b", col="blue", xlab="Taille de l'échantillon", ylab="Biais", main="Biais de beta en fonction de la taille de l'échantillon")
+lines(abcis, B_tot_mom_mu, col="red")
+legend("topright", legend = c("likely", "moment"), col = c("blue", "red"), lty = 1)
+
+
+# Tracé des biais pour mu
+plot(abcis, B_tot_mu, type="b", col="blue", xlab="Taille de l'échantillon", ylab="Biais", main="Biais de mu en fonction de la taille de l'échantillon")
+lines(abcis, B_tot_mom_beta, col="red")
+legend("topright", legend = c("likely", "moment"), col = c("blue", "red"), lty = 1)
+
+
+# Tracé de la variance pour beta
+plot(abcis, V_tot, type="b", col="blue", xlab="Taille de l'échantillon", ylab="Variance", main="Variance de beta en fonction de la taille de l'échantillon")
+lines(abcis, Var_tot_mom_beta, col="red")
+legend("topright", legend = c("likely", "moment"), col = c("blue", "red"), lty = 1)
+
+
+# Tracé de la variance pour mu
+plot(abcis, V_tot_mu, type="b", col="blue", xlab="Taille de l'échantillon", ylab="Variance", main="Variance de mu en fonction de la taille de l'échantillon")
+lines(abcis, Var_tot_mom_mu, col="red")
+legend("topright", legend = c("likely", "moment"), col = c("blue", "red"), lty = 1)
+
+
+
 B_V_tot <-c(B_tot[1]^2 + V_tot[1],B_tot[2]^2 + V_tot[2],B_tot[3]^2 + V_tot[3],B_tot[4]^2 + V_tot[4],B_tot[5]^2 + V_tot[5])
-
 plot(abcis, B_V_tot, type="b", col="blue", xlab="Taille de l'échantillon", ylab="MSE", main="biais de beta carré + variance de beta")
 
 
